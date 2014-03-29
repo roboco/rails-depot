7c10ae006b00d5a23e2a5b69ca406df64415999d
Robin Andrew <robin@cablestudios.co.uk>
2014-03-29 15:16:37 +0000

    recentupdates

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,13 +1,21 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
     @update = {
-      title: 'Lorem Ipsum',
-      description: 'wibbles are fun!',
-      image_url: 'lorem.jpg',
-      price: 19.95
+      title:       'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url:   'lorem.jpg',
+      price:       19.95
     }
   end
 
@@ -45,8 +53,16 @@ class ProductsControllerTest < ActionController::TestCase
     assert_redirected_to product_path(assigns(:product))
   end
 
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete :destroy, id: products(:ruby)
+    end
+
+    assert_redirected_to products_path
+  end
+
   test "should destroy product" do
-  assert_difference('Product.count', -1) do
+    assert_difference('Product.count', -1) do
       delete :destroy, id: @product
     end
 
